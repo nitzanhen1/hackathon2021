229bbb1107a1fd09bca31c366d129b954bfc4e2d
nitzanhen1 <nitzanhe@post.bgu.ac.il>
2021-12-29 21:51:25 +0200

    it's alive!

--- client.py
+++ client.py
@@ -2,75 +2,48 @@
 
 import random
 import struct
-import sys
-
-import select
-import time
-import traceback
-from struct import *
 from socket import *
-import enum
-import msvcrt
-#from msvcrt import getch
-#from msvcrt import getche
 from threading import Thread
-import signal
-#import keyboard
-#from scapy.all import *
 
-
-
-# CLIENT_IP = get_if_addr("eth0") # 172.1.0 (eth1)
-# CLIENT_IP = '172.1.0/24'
-CLIENT_IP = '172.0.0.1'
-# CLIENT_IP = gethostbyname(gethostname())  # 172.99.0 (eth2)
 localPORTUDP = 13117
-#localPORTTCP = 2037
 BUFFER_SIZE = 2048
 MAGIC_COOKIE = 0xabcddcba
 MSG_TYPE = 0x2
 
 class Client:
     def __init__(self):
-        self.team_name = "PD"+ str(random.randint(0,10))
-        self.ip = CLIENT_IP
+        self.team_name = "PyCharmers"
         self.udp_socket = socket(AF_INET, SOCK_DGRAM)
-        self.tcp_socket = socket(AF_INET, SOCK_STREAM)#?
-        self.game_is_on = False
+        self.tcp_socket = socket(AF_INET, SOCK_STREAM)
+        #self.udp_socket.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+        self.udp_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+        self.udp_socket.bind(('', localPORTUDP))
+        #self.udp_socket.settimeout(1)
 
     def connect_to_server(self, server_ip, server_port):
         """
         This function sends a connection request to the server.
         """
-        #self.tcp_socket.bind(('', localPORTTCP))
-        self.tcp_socket.connect((server_ip, server_port))
-        print("Connected to Server")
+        try:
+            self.tcp_socket.connect((server_ip, server_port))
+            print("Connected to Server")
+        except:
+            return
 
     def send_name(self):
-        msg = self.team_name + '\n'
-        self.tcp_socket.send(msg.encode())
-        print("send"+ str(msg))
-
-    def send_to_server(self, event):
-        """
-        This function sends an event to the server.
-        :param event: event to send
-        """
         try:
-            print(event.name)
-            self.tcp_socket.send(event.name.encode())
+            msg = self.team_name + '\n'
+            self.tcp_socket.send(msg.encode())
         except:
-            print("something went wrong - with key pressing sending from CLIENT to SERVER")
+            return
 
     def start_run(self):
         """
         This function lets the client to listen for boradcast from the server,
         and then sends the Client's Team Name to the Server
         """
+        self.tcp_socket = socket(AF_INET, SOCK_STREAM)
         print("Client Started, listening for offer requests...")
-        self.udp_socket.setsockopt(SOL_SOCKET, SO_BROADCAST, 1) #TODO try to delete
-        self.udp_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1) #TODO change to port
-        self.udp_socket.bind(('', localPORTUDP))  # bind socket to local port number ??
 
         while True:
             try:
@@ -83,13 +56,11 @@ class Client:
                     continue
                 print(f'Received offer from {server_address[0]}, attempting to connect...')
                 self.connect_to_server(server_address[0], server_port)
-                self.send_name() #TODO: what if client doesnt send name?
+                self.send_name()
                 self.connected()
-
-                break
+                break;
             except:
                 continue
-        self.udp_socket.close()
 
     def connected(self):
         while True:  # game
@@ -99,57 +70,30 @@ class Client:
                 print(question)
                 break
             except:
-                print('connect bad')
-                self.tcp_socket.close()  # why?
-                break
-        # go into game
-
-        timeout = 5
-        startTime=  time.time()
-        inp = None
-        while True:
-            if msvcrt.kbhit():
-                inp = msvcrt.getch.getch()
-                print("inp")
-                break
-            elif time.time() - startTime > timeout:
-                print("inp timeout")
-                break
-
-        print(inp)
-        #gameThread = Thread(target=self.playGame, args=())
-        #gameThread.start()
-
+                continue
+        play_thread = Thread(target=self.playGame, args=())
+        play_thread.start()
         while True:
             try:
                 # server game over msg
                 winner = self.tcp_socket.recv(BUFFER_SIZE).decode()
                 print(winner)
+                self.tcp_socket.close()
+                print("server disconnected, listening for offer requests...")
                 break
             except:
                 continue
 
+
+
     def playGame(self):
-        """
-        def nothing(sig, frame):pass
-        print("play game")
-        signal.signal(signal.SIGALRM,nothing)
-        signal.alarm(5)
         try:
-            print("before in")
-            ans = input()
-            print("after in")
-            signal.alarm(0)
-            #ans = getche()
-            print(ans)
-            print("input timeout")
+            ans = input()[0]
             self.tcp_socket.send(ans.encode())
         except:
-            print('game bad')
-            #self.tcp_socket.close()  # why?
-        """
-
+            return
 
 
 client=Client()
-client.start_run()
+while True:
+    client.start_run()
--- server.py
+++ server.py
@@ -9,9 +9,9 @@ from threading import *
 import struct
 import random
 
-SERVER_IP = gethostbyname(gethostname())
-UDP_SERVER_PORT = 2037  # //TODO: random?
-TCP_SERVER_PORT = 12177   #TODO: check?
+SERVER_IP = gethostbyname(gethostname()) #'172.99.0.37'
+UDP_SERVER_PORT = 2037
+TCP_SERVER_PORT = 12177
 UDP_DEST_PORT = 13117
 BUFFER_SIZE = 2048
 MAGIC_COOKIE = 0xabcddcba
@@ -22,17 +22,21 @@ class Server:
     def __init__(self):
         self.udp_socket = socket(AF_INET, SOCK_DGRAM)
         self.tcp_socket = socket(AF_INET, SOCK_STREAM)
+        self.udp_socket.bind(('', UDP_SERVER_PORT))
+        self.udp_socket.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)  # as is
+        self.udp_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+        self.tcp_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+        self.tcp_socket.bind(('', TCP_SERVER_PORT))
+        #self.tcp_socket.listen(20)
+        self.tcp_socket.settimeout(1)
+        self.answers = []
         self.players_names = []
         self.players_sockets = []
-        self.players_addr = []
-        self.game_treads = {}#?
-        self.players = {}#?
-        self.answers =[]
-        self.equations = {'1+1':'2', '1+2':'3'}#TODO change to dict+change the questions
+        self.question = ''
+        self.answer = ''
 
     def send_broadcast_messages(self, udp_socket):
-        print("in broadcast")
-        print(f"Server started, listening on IP address {SERVER_IP}")  #TODO where?
+        print(f"Server started, listening on IP address {SERVER_IP}")
         message_to_send = struct.pack('Ibh', MAGIC_COOKIE, MSG_TYPE, TCP_SERVER_PORT)
         while len(self.players_names) < 2:
             udp_socket.sendto(message_to_send, ('<broadcast>', UDP_DEST_PORT))
@@ -40,76 +44,64 @@ class Server:
 
 
     def accept_conn(self, broadcast_thread, tcp_socket):
-        print("in accept")
-        while broadcast_thread.is_alive() and len(self.players_names)<2:
-            print(len(self.players_names))
+        while len(self.players_names)<2: #broadcast_thread.is_alive() and
             try:
                 client_socket, client_address = tcp_socket.accept()
                 player = client_socket.recv(2048).decode()
                 self.players_names.append(player)
-                print(player)
                 self.players_sockets.append(client_socket)
-                self.players_addr.append(client_address)
-                print("client connected:"+ str(player)+" sock: "+ str(client_socket)+" addr: "+str(client_address))
-            except timeout:
-                # traceback.print_exc()
+            except:
                 continue
-        time.sleep(3)# TODO 10 sec
 
-        questions = list(self.equations)
-        inx = random.randint(0, len(questions) - 1)
-        q1 = questions[inx]
-        ans = self.equations[q1]
+        time.sleep(10)
+        self.go_question()
+        q1 = self.question
+        ans = self.answer
         player1_conn = Thread(target=self.playGame, args=(self.players_sockets[0],self.players_names[0],q1))
         player2_conn = Thread(target=self.playGame, args=(self.players_sockets[1],self.players_names[1],q1))
+
         self.answers=[]
         player1_conn.start()
         player2_conn.start()
+        player1_conn.join(5)
+        player2_conn.join(5)
 
-        TIMEOUT= 10
-        t_start = time.time()
-        while (time.time() - t_start < 10 and not event.is_set()):
-            time.sleep(0.5)
-
-        #player1_conn.join()
-        #player2_conn.join()
-        print(self.answers)
+        #TIMEOUT= 10
+        #t_start = time.time()
+        #while (time.time() - t_start < 10 and not event.is_set()):
+        #    time.sleep(0.5)
 
         winner = "Game Over!\n"
-        winner += "The correct answer was " + ans+"!\n"
+        winner += "The correct answer was " + self.answer+"!\n"
         if(len(self.answers)==0):
             winner+= "The game ended in a Draw!"
         else:
             winner += "Congratulations to the winner:\n"
-            if self.answers[0][0]==ans:
+            if self.answers[0][0]==self.answer:
                 winner += self.answers[0][1]
             else:
-                if(self.answers[0][1]==self.players_names[0]):
+                if self.answers[0][1]==self.players_names[0]:
                     winner+=self.players_names[1]
                 else:
                     winner+=self.players_names[0]
-        print(winner)
-
-        print(self.answers)
 
         try:
             self.players_sockets[0].send(winner.encode())
         except:
             print(f" disconnected: {self.players_names[0]}")
-        #self.players_sockets[0].close()
 
         try:
             self.players_sockets[1].send(winner.encode())
         except:
             print(f" disconnected: {self.players_names[1]}")
-        #self.players_sockets[1].close()
 
         print("Game over, sending out offer requests...")
 
-
-        while True:#TODO: delete
-            print("end game")
-            time.sleep(4)
+        self.players_sockets[0].close()
+        self.players_sockets[1].close()
+        self.players_names = []
+        self.answers =[]
+        self.players_sockets = []
 
     def playGame(self,player_socket, player_name,q1):
         msg = "Welcome to Quick Maths.\n"
@@ -118,18 +110,27 @@ class Server:
         msg += "==\n"
         msg += "Please answer the following question as fast as you can:\n"
         msg += "How much is "+str(q1)+"?"
-        player_socket.send(msg.encode())
 
-        player_socket.settimeout(10)
         try:
+            player_socket.send(msg.encode())
+            player_socket.settimeout(10)
             ans_client = player_socket.recv(BUFFER_SIZE).decode()
             self.answers.append((ans_client,player_name))
             event.set()
-            print(ans_client)
-            print(player_name)
-            # if ans_client[0] == ans:
-        except timeout:
-            print("timeout")
+        except:
+            return
+
+    def go_question(self):
+        opp = random.choice(['+', '-'])
+        ans = random.randint(0, 9)
+        if opp == '+':
+            op1 = random.randint(1, ans)
+            op2 = ans - op1
+        else:
+            op1 = random.randint(ans, 100)
+            op2 = op1 - ans
+        self.question = str(op1) + opp + str(op2)
+        self.answer = str(ans)
 
 
     def waiting_for_clients(self):
@@ -137,31 +138,22 @@ class Server:
             This function sends UDP broadcast messages each 1 sec
             for 10 seconds and listening for clients responses.
         """
-        self.udp_socket.bind((SERVER_IP, UDP_SERVER_PORT))
-        self.udp_socket.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)  # as is
-        self.udp_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-        self.tcp_socket.bind((SERVER_IP, TCP_SERVER_PORT))
-        self.tcp_socket.listen(20)
-        self.tcp_socket.settimeout(1) # ???
+        try:
+            self.tcp_socket.listen(20)
+        except:
+            return
         broadcast_thread = Thread(target=self.send_broadcast_messages, args=(self.udp_socket,))
         accpt_conn_thread = Thread(target=self.accept_conn, args=(broadcast_thread, self.tcp_socket))
         broadcast_thread.start()
         accpt_conn_thread.start()
-        # broadcast_thread.join() #????
+        broadcast_thread.join()
         accpt_conn_thread.join()
-        self.udp_socket.close()
-        self.tcp_socket.close()
 
 
 def Main():
     server = Server()
     while True:
         server.waiting_for_clients()
-        #t1 = threading.Thread(target=acceptThreadFunction, args=(server,))
-        #t1.start()
-        #server.waitForClients()
-        #server.manageGame()
-
 
 if __name__ == '__main__':
     Main()
